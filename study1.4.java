89e44e32867f17df711942f386ba35af4ee48fc5
KoudouMizuki <k.mizuki0706@gmail.com>
2023-07-22 14:57:46 +0000

    Java学習

---
+++ study1.4.java
@@ -0,0 +1,11 @@
+class Main {
+  public static void main(String[] args) {
+    int number;
+    number = 3;
+    System.out.println(number);
+    
+    String name;
+    name = "Wanko";
+    System.out.println(name);
+  }
+}
---
+++ study_java/study1.2.java
@@ -0,0 +1,10 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println(17);
+    System.out.println(5 + 3);
+    System.out.println("5 + 3");
+    System.out.println(12 / 3);
+    System.out.println(3 * 6);
+    System.out.println(8 % 3);
+  }
+}
