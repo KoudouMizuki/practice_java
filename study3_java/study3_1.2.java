bd4116a8af935feeff3e59e25d30410125573fdb
KoudouMizuki <k.mizuki0706@gmail.com>
2023-08-22 10:12:41 +0000

    Java学習

---
+++ study3_java/study3_1.2.java
@@ -0,0 +1,14 @@
+class Main {
+  public static void main(String[] args) {
+    // printDataメソッドを呼び出してください
+    printDAta();
+    
+  }
+  
+  // printDataメソッドを定義してください
+  public static void printData() {
+    System.out.println("私の名前はKate Jonesです");
+  }
+  printDAta();
+  
+}
